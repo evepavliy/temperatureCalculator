9ec82753154226f17f5307c334fbe9199a3354b4
Ievgeniia Pavliuchyk <ievgeniia.pavliuchyk@gmail.com>
2019-02-17 01:32:20 +0100

    Initial Commit

---
+++ Temperature CalculatorTests/Temperature_CalculatorTests.swift
@@ -0,0 +1,34 @@
+//
+//  Temperature_CalculatorTests.swift
+//  Temperature CalculatorTests
+//
+//  Created by Ievgeniia Pavliuchyk on 17/02/2019.
+//  Copyright © 2019 Ievgeniia Pavliuchyk. All rights reserved.
+//
+
+import XCTest
+@testable import Temperature_Calculator
+
+class Temperature_CalculatorTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
