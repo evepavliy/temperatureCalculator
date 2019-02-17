2fef9a3e0d5fbf07aeeeb9f502aef61501924bd4
Ievgeniia Pavliuchyk <ievgeniia.pavliuchyk@gmail.com>
2019-02-17 02:10:30 +0100

    Segment control supports change between working in C and F

--- Temperature Calculator/ViewController.swift
+++ Temperature Calculator/ViewController.swift
@@ -10,11 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var enterLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var outputLabel: UILabel!
+    @IBOutlet weak var segmentControl: UISegmentedControl!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func calculate(_ sender: Any) {
+    }
+    
+    @IBAction func formatCalculation(_ sender: Any) {
+        
+        if segmentControl.selectedSegmentIndex == 0 {
+            enterLabel.text = "Enter Fahrenheit"
+            outputLabel.text = "O Celsius"
+            textField.text = ""
+            
+        }
+        
+        if segmentControl.selectedSegmentIndex == 1 {
+            enterLabel.text = "Enter Celsius"
+            outputLabel.text = "O Fahrenheit"
+            textField.text = ""
+            
+        }
+        
+    }
 }
 
